b06617a18a802a9192d1d5c8332c54666c584b76
GLY270 <glyzafaithsidaya@gmail.com>
2024-11-05 14:40:03 +0800

    Add files via upload

--- sidaya,gf.py
+++ sidaya,gf.py
@@ -2,14 +2,14 @@ from tkinter import*
 import sqlite3
 
 root=Tk()
-root.title('Personal Info')
+root.title('Personal info')
 root.geometry("500x500")
 
-conn=sqlite3.connect('personal_info.db')
+conn=sqlite3.connect('Personal_info.db')
 c=conn.cursor()
 
 def submit():
-    conn=sqlite3.connect('C:/Users/STUDENT6/Desktop/Personal _info.db')
+    conn=sqlite3.connect('C:/Users/STUDENTS/Desktop')
     c=conn.cursor()
     c.execute ("INSERT INTO Student_info VALUES(:f_name,:l_name,:age,:address,:email)",
               {
@@ -29,7 +29,7 @@ def submit():
     email.delete(0,END)
 
 def query():
-    conn=sqlite3.connect('C:/Users/STUDENT6/Desktop/Personal _info.db')
+    conn=sqlite3.connect('C:/Users/STUDENTS/Desktop')
     c=conn.cursor()
     c.execute("SELECT*,oid FROM Student_info")
     records=c.fetchall()
@@ -43,14 +43,39 @@ def query():
 
     conn.commit()
     conn.close()
-    
-    
+
+def delete():
+   conn=sqlite3.connect('C:/Users/STUDENTS/Desktop')
+   c=conn.cursor()
+   c.execute("Delete from Student_info WHERE oid="+delete_box.get())
+
+
+   def edit():
+       editor=Tk()
+       editor.title('Update Record from database')
+       editor.geometry("500x500")
+
+       conn=sqlite3.connect('C:/Users/STUDENT6/Desktop/Personal_info.db')
+
+       c=conn.cursor()
+
+   record_id=delete_box.get()
+   c.execute("SELECT * FROM student_info WHERE oid="+record_id)
+   records=c.fetchall()
+
+
+   for record in records:
+      f_name_editor.insert(0,record[0])
+      l_name_editor.insert(0,record[1])
+      age_editor.insert(0,record[2])
+      address_editor.insert(0,record[3])
+      gmail_editor.insert(0,record[4])  
    
 '''
 
-c.execute("""CREATE TABLE "Student_info" (
-	"f_name"	TEXT,
-	"l_name"	TEXT,
+c.execute("""CREATE TABLE "Personal_info" (
+	"fname"	TEXT,
+	"lname"	TEXT,
 	"age"	INTEGER,
 	"address"	TEXT,
 	"email"	TEXT
@@ -84,6 +109,15 @@ submit_btn.grid(row=6,column=0,columnspan=2,pady=10,padx=10,ipadx=100)
 
 query_btn=Button(root,text="Show records",command=query)
 query_btn.grid(row=7,column=0,columnspan=2,pady=10,padx=10,ipadx=137)
+
+query_btn=Button(root,text="Delete Records",command=delete)
+query_btn.grid(row=12,column=0,columnspan=2,pady=10,padx=10,ipadx=136)
+
+delete_box=Entry(root,width=30)
+delete_box.grid(row=10, column=1, padx=30)
+delete_box_label=Label(root,text="Select ID No.")
+delete_box_label.grid(row=10,column=0)
+
 
 root.mainloop()
         
